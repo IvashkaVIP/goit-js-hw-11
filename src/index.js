d39db8ae047b0f77eccba7f9d989e639ac21dc24
IvashkaVIP <ivashka.co@gmail.com>
2023-05-27 13:08:49 +0300

    add querySelectors

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import './sass/index.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchPictures } from './sass/fetchPictures';
+
+//const DEBOUNCE_DELAY = 300;
+
+
+const btnSubmit = document.querySelector('#search-form')
+
+
+// const countryList = document.querySelector('.country-list');
+// const countryInfo = document.querySelector('.country-info');
+
+btnSubmit.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(evt) {
+    evt.preventDefault();
+    const searchQuery = document.querySelector('input');
+    console.log(searchQuery.value.trim());  
+}
+
+//searchQuery.addEventListener('input', debounce(inputCountries, DEBOUNCE_DELAY));
+//console.log(searchQuery);
