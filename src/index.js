495f34bc6ed7f398dec957e30805b0d2d1e5dd4e
IvashkaVIP <ivashka.co@gmail.com>
2023-05-30 19:52:37 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,78 +1,36 @@
 import './sass/index.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-//import debounce from 'lodash.debounce';
-import { fetchPictures } from './sass/fetchPictures';
-// Описан в документации
 import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { creatMarkupPictures } from './import/creatMarkup';
+import { fetchPictures } from './import/fetchPictures';
+import { lightbox } from './import/lightBox';
 
 const HITS_PER_PAGE = 40;
-//const DEBOUNCE_DELAY = 300;
 
 const btnSubmit = document.querySelector('#search-form');
 const searchQuery = document.querySelector('input');
 const picturesList = document.querySelector('.gallery');
-const loadmore = document.querySelector('.js-load-more');
 const guard = document.querySelector('.js-guard');
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: '250',
-});
-
-const options = {
-  root: null,
-  rootMargin: '400px',
-  threshold: 1.0,
-};
-
-const observer = new IntersectionObserver(handlerPagination, options);
 
 btnSubmit.addEventListener('submit', onBtnSubmit);
 
 let page = 1;
 let inputQuery = '';
 
+const options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(handlerPagination, options);
 
 function formDisabled(isDisabled) {
   btnSubmit[0].disabled = isDisabled;
   btnSubmit[1].disabled = isDisabled;
 }
-function creatMarkupPictures(data) {
-  return data.hits
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-                  <a href="${largeImageURL}">
-                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                  </a>
-                      <div class="info">
-                        <p class="info-item">
-                        <b>Likes</b>${likes}
-                        </p>
-                        <p class="info-item">
-                        <b>Views</b>${views}
-                        </p>
-                        <p class="info-item">
-                        <b>Comments</b>${comments}
-                        </p>
-                        <p class="info-item">
-                        <b>Downloads</b>${downloads}
-                        </p>
-                      </div>
-                </div>`;
-      }
-    )
-    .join('');
-}
+
 function clearQery() {
   page = 1;
   inputQuery = '';
@@ -94,7 +52,6 @@ function smoothScroll(element) {
 }
 
 async function onBtnSubmit(evt) {
-  
   evt.preventDefault();
   clearQery();
   inputQuery = searchQuery.value.trim();
@@ -156,10 +113,3 @@ function handlerPagination(entries, observer) {
  
 }
 
-
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-//searchQuery.addEventListener('input', debounce(inputCountries, DEBOUNCE_DELAY));
-//console.log(searchQuery);
